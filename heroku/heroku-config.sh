4bcb44fb660ce35a1dab60f080ca23bfb0ba2e85
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-04-12 00:39:35 -0400

    adds heroku config dynamically

--- heroku/heroku-config.sh
+++ heroku/heroku-config.sh
@@ -1,14 +1,22 @@
 # /bin/bash
+heroku create --buildpack "https://github.com/HashNuke/heroku-buildpack-elixir.git"
+heroku buildpacks:add https://github.com/gjaldon/heroku-buildpack-phoenix-static.git
+
 
 # Fix prod.exs
-cp prod.exs ../../config/prod.exs
-echo "copied prod.exs";
+sed '16s/.*/url: [scheme: "https", host: "$(heroku info -s | grep web_url | cut -d= -f2)", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],' $(pwd)/config/prod.exs
 
-# Add socket timeout
-sed '8s/.*/transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000/' /Users/karavanm/end2end/_sandbox/some_shit_9/web/channels/user_socket.ex
-
+# Copy Profile
 cp Procfile ../../Procfile
 echo "copied Procfile";
 
+# Copy elixir_buildpack
 cp elixir_buildpack.config ../../elixir_buildpack.config
 echo "copied elixir_buildpack";
+
+# Go to root directory
+cd ../../
+
+# Add socket timeout
+sed '8s/.*/transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000/' $(pwd)/web/channels/user_socket.ex
+
