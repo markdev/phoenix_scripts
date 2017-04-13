134c6b4b467265b6dc7157688b02cae7865ae5f1
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-04-13 01:56:32 -0400

    fixes sed shit

--- simple_auth/simple_auth.sh
+++ simple_auth/simple_auth.sh
@@ -5,15 +5,15 @@ cd ../..
 # Create the user model
 mix phoenix.gen.model User users email:string name:string password_hash:string is_admin:boolean
 # add null: false
-sed -ie '6s/$/, null: false/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
+sed -i '' '6s/$/, null: false/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
 # add create unique_index
-sed -ie '11s/$/\
+sed -i '' '12s/$/\
       create unique_index(:users, [:email])/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
 
 # Create posts model
 mix phoenix.gen.model Post posts title:string body:text user_id:references:users
 
 # Run migrations
-mix ecto.migrate
+#mix ecto.migrate
 
 cd -   
