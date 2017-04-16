421c5c603e78fe3ca9c7d2d0a20c103351a1fc85
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-04-15 23:32:51 -0400

    adds simple_auth_2 scripts

--- simple_auth_2/config/config.exs
+++ simple_auth_2/config/config.exs
@@ -7,20 +7,20 @@ use Mix.Config
 
 # General application configuration
 config :simple_auth,
-  ecto_repos: [SimpleAuth.Repo]
+  ecto_repos: [MyApplication.Repo]
 
 # Configures the endpoint
-config :simple_auth, SimpleAuth.Endpoint,
+config :simple_auth, MyApplication.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "yAz5RhEjmtbvahJzAg3qhBb3U1qQBUXmTpsTIrDO5EPQffTvZgMLHmIRCRv47rnj",
-  render_errors: [view: SimpleAuth.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: SimpleAuth.PubSub, adapter: Phoenix.PubSub.PG2]
+  render_errors: [view: MyApplication.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: MyApplication.PubSub, adapter: Phoenix.PubSub.PG2]
 
 config :guardian, Guardian,
- issuer: "SimpleAuth.#{Mix.env}",
+ issuer: "MyApplication.#{Mix.env}",
  ttl: {30, :days},
  verify_issuer: true,
- serializer: SimpleAuth.GuardianSerializer,
+ serializer: MyApplication.GuardianSerializer,
  secret_key: to_string(Mix.env) <> "SuPerseCret_aBraCadabrA"
 
 # Configures Elixir's Logger
--- simple_auth_2/simple_auth_2.sh
+++ simple_auth_2/simple_auth_2.sh
@@ -7,20 +7,62 @@ SEDSTRTWO=$(sed '33q;d' web/web.ex)
 UPPER=$(echo $SEDSTRTWO | awk -F 'alias|Repo' '{print $2}' | sed 's/.$//')
 cd -
 
+
 # Battle plan
 # 1. Migrations (user and post)
-# 2. mix.exs Dependencies (comeonin and guardian)
-# 3. config
-# 4. seeds
-# 5. routes
-# 6. mkdir -p web/files
-# 6b. cp web/files
-# 6a. sed web/files
+# Create the user model
+cd ../..
+mix phoenix.gen.model User users email:string name:string password_hash:string is_admin:boolean
+# add null: false
+sed -i '' '6s/$/, null: false/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
+# add create unique_index
+sed -i '' '12s/$/\
+      create unique_index(:users, [:email])/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
+mix phoenix.gen.model Post posts title:string body:text user_id:references:users
+cd -
+echo "Completed -- 1: Migrations";
 
-# 7. migrate
-# 8. mix deps.get
-# 9. seed
-# 10. mix phoenix.server
+
+# 2. mix.exs Dependencies (comeonin and guardian)
+# Add comeonin dependency
+sed -i '' "22s|:postgrex|:postgrex, :comeonin|g" $(pwd)/../../mix.exs
+sed -i '' '40s|]$|,\
+     {:comeonin, "~> 2.5"}]|g' $(pwd)/../../mix.exs
+# Add guardian dependency
+sed -i '' '41s|]$|,\
+     {:guardian, "~> 0.12.0"}]|g' $(pwd)/../../mix.exs
+echo "Completed -- 2: Dependencies";
+
+
+# 3. config
+# Add config/config.exs
+cp config/config.exs $(pwd)/../../config/config.exs
+sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../config/config.exs
+echo "Completed -- 3: config";
+
+# 4. seeds
+echo "TODO: -- 4: seeds";
+
+# 5. mkdir -p web/files
+for f in $(find web | awk '!/.eex/ && !/.ex/'); 
+	do 
+		mkdir -p "../../"${f}; 
+	done
+
+# 5b. cp web/files
+for f in $(find web | awk '/.eex/ || /.ex/'); 
+	do 
+		cp ${f} "../../"${f}; 
+	done
+
+# 5a. sed web/files
+perl -pi -e "s/MyApplication/${UPPER}/g" `find ../../web -name "*.ex" -or -name "*.eex"`
+echo "Completed -- 5: mv web files";
+
+# 6. migrate
+# 7. mix deps.get
+# 8. seed
+# 9. mix phoenix.server
 
 
 
@@ -332,15 +374,15 @@ cd -
 #             <% end %>/g' $(pwd)/../../web/templates/layout/app.html.eex 
 ###################################
 
-cp web/auth/auth.ex $(pwd)/../../web/auth/auth.ex
-sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../web/auth/auth.ex
+# cp web/auth/auth.ex $(pwd)/../../web/auth/auth.ex
+# sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../web/auth/auth.ex
 
-sed -i '' '12s/.*/    conn\
-    |> MyApplication.Auth.logout\
-    |> put_flash(:info, "See you later!")\
-    |> redirect(to: page_path(conn, :index))\
-/g' $(pwd)/../../web/controllers/session_controller.ex
-sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../web/controllers/session_controller.ex
+# sed -i '' '12s/.*/    conn\
+#     |> MyApplication.Auth.logout\
+#     |> put_flash(:info, "See you later!")\
+#     |> redirect(to: page_path(conn, :index))\
+# /g' $(pwd)/../../web/controllers/session_controller.ex
+# sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../web/controllers/session_controller.ex
 
 
 
