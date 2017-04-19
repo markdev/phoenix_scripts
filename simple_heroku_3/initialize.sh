fe2b31b58451fd11478a110ce23818aca99f1cfb
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-04-19 01:33:23 -0400

    back to basics

--- simple_heroku_3/initialize.sh
+++ simple_heroku_3/initialize.sh
@@ -35,78 +35,15 @@ cd -
 ######
 
 
-
-
-
-######## Simple Auth
 cd ../..
-SEDSTRONE=$(sed '9q;d' config/config.exs)
-LOWER=$(echo $SEDSTRONE | awk -F ':|,' '{print $2}')
+mix phoenix.gen.html User users name:string email:string
+git add . && git commit -m "Add generated User model"
 
-SEDSTRTWO=$(sed '33q;d' web/web.ex) 
-UPPER=$(echo $SEDSTRTWO | awk -F 'alias|Repo' '{print $2}' | sed 's/.$//')
-cd -
+sed -i '' '19s|$|\
+\
+		resources "/users", UserController|g' $(pwd)/web/router.ex
+git add . && git commit -m "Add Users resource to browser scope"
 
-
-# Battle plan
-# 1. Migrations (user and post)
-# Create the user model
-cd ../..
-mix phoenix.gen.model User users email:string name:string password_hash:string is_admin:boolean
-# add null: false
-sed -i '' '6s/$/, null: false/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
-# add create unique_index
-sed -i '' '12s/$/\
-      create unique_index(:users, [:email])/' $(pwd)/$(find priv/repo/migrations/ -name "*create_user*")
-mix phoenix.gen.model Post posts title:string body:text user_id:references:users
-cd -
-echo "Completed -- 1: Migrations";
-
-
-# 2. mix.exs Dependencies (comeonin and guardian)
-# Add comeonin dependency
-sed -i '' "22s|:postgrex|:postgrex, :comeonin|g" $(pwd)/../../mix.exs
-sed -i '' '40s|]$|,\
-     {:comeonin, "~> 2.5"}]|g' $(pwd)/../../mix.exs
-# Add guardian dependency
-sed -i '' '41s|]$|,\
-     {:guardian, "~> 0.12.0"}]|g' $(pwd)/../../mix.exs
-echo "Completed -- 2: Dependencies";
-
-
-# 3. config
-# Add config/config.exs
-cp config/config.exs $(pwd)/../../config/config.exs
-sed -i '' "s|my_application|${LOWER}|g" $(pwd)/../../config/config.exs
-sed -i '' "s|MyApplication|${UPPER}|g" $(pwd)/../../config/config.exs
-echo "Completed -- 3: config";
-
-# 4. seeds
-echo "TODO: -- 4: seeds";
-
-# 5. mkdir -p web/files
-for f in $(find web | awk '!/.eex/ && !/.ex/'); 
-	do 
-		mkdir -p "../../"${f}; 
-	done
-
-# 5b. cp web/files
-for f in $(find web | awk '/.eex/ || /.ex/'); 
-	do 
-		cp ${f} "../../"${f}; 
-	done
-
-# 5a. sed web/files
-perl -pi -e "s/MyApplication/${UPPER}/g" `find ../../web -name "*.ex" -or -name "*.eex"`
-echo "Completed -- 5: mv web files";
-
-
-
-
-
-
-cd ../..
-mix deps.get
 mix ecto.create
 mix ecto.migrate
 
