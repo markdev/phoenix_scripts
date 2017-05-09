0ce516e8ad72b395de439c7eb0f0474c47d164b6
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-05-08 22:45:07 -0400

    adds ex_admin install

--- model1/initialize.sh
+++ model1/initialize.sh
@@ -3,9 +3,15 @@
 # Must start at the initialize directory
 cd ../..
 
-# Set script variables
+# # Set script variables
 MYDIR=$(pwd)
 PSCRIPTPATH=$(echo "$MYDIR"/phoenix_scripts/model1)
+
+SEDSTRONE=$(sed '9q;d' config/config.exs)
+LOWER=$(echo $SEDSTRONE | awk -F ':|,' '{print $2}')
+
+SEDSTRTWO=$(sed '33q;d' web/web.ex) 
+UPPER=$(echo $SEDSTRTWO | awk -F 'alias|Repo' '{print $2}' | sed 's/.$//')
 
 # Hide phoenix_scripts
 echo "" >> .gitignore
@@ -26,16 +32,60 @@ cp "$PSCRIPTPATH"/index.html.eex "$MYDIR"/web/templates/page/index.html.eex
 cp "$PSCRIPTPATH"/package.json "$MYDIR"/package.json
 cd "$MYDIR" && git add . && git commit -m "Adds Tiansu front-end configuration"
 
-# In here shall go the mix deps.get dependency
-# https://stackoverflow.com/questions/38972736/how-to-select-lines-between-two-patterns
-
 # Installations
 mix ecto.create
 mix ecto.migrate
 npm install
 
-# Here shall go the users and user auth
+# Add ex_admin dependency
+# TODO: {:ex_admin, "~> 0.8"} with override
+sed -i  '' 's/}]/},\
+   	  {:ex_admin, github: "smpallen99/ex_admin"}]/g' "$MYDIR"/mix.exs
 
-# email
+# Add ex_admin config
+echo "" >> "$MYDIR"/config/config.exs
+echo "config :ex_admin," >> "$MYDIR"/config/config.exs
+echo "repo: $UPPER.Repo," >> "$MYDIR"/config/config.exs
+echo "module: $UPPER," >> "$MYDIR"/config/config.exs    
+echo "modules: [" >> "$MYDIR"/config/config.exs
+echo "  $UPPER.ExAdmin.Dashboard," >> "$MYDIR"/config/config.exs
+echo "]" >> "$MYDIR"/config/config.exs
 
-# avatar/ex_aws
+# Install ex_admin
+mix do deps.get, deps.compile
+mix admin.install
+git add . && git commit -m "installs ex_admin"
+
+# Add ex_admin routes
+sed -i '' '21s|$|\
+  # your apps routes\
+  scope "/admin", ExAdmin do\
+    pipe_through :browser\
+\
+    admin_routes()\
+  end|g' "$MYDIR"/web/router.ex
+
+sed -i '' '2s|$|\
+  use ExAdmin.Router|g' "$MYDIR"/web/router.ex
+
+# Add ex_admin paging configuration
+sed -i '' '2s|$|\
+  use Scrivener, page_size: 10|g' "$MYDIR"/lib/"$LOWER"/repo.ex
+
+# Add brunch-config.js  
+echo "Now it's time to fix brunch-config.js"
+echo "Open brunch-config.js and apply the changes at the bottom"
+echo "Are you finished? [Yes|No]"
+read FINISHED
+
+# launch server
+echo "Ok, now let's test it!"
+iex -S mix phoenix.server
+
+
+
+# # Here shall go the users and user auth
+
+# # email
+
+# # avatar/ex_aws
